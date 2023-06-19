6bcbb1b623133f1e810a1f6b68905508023f1c81
LLIIILIL <114857618+LLIIILIL@users.noreply.github.com>
2023-06-19 13:45:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telethon import TelegramClient, events, Button, errors
 from telethon.tl.functions.channels import JoinChannelRequest
 from telethon.tl.functions.messages import ImportChatInviteRequest
-from telethon.kayon import StringSession
+from telethon.sessions import StringSession
 import asyncio, json, os, re
 # session @xsiner0
 api_id_bot = 24106627 # اب ايدي 
